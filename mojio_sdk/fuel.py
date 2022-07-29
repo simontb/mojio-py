71c15386e577a7615619d777290b4b30dfd61d56
Karl Moos <karlmoos@gmail.com>
2022-07-29 08:14:21 -0500

    Fuel fixes

--- mojio_sdk/fuel.py
+++ mojio_sdk/fuel.py
@@ -6,12 +6,15 @@ class Fuel:
         self.fuel_level = 0.0
         self.virtual_fuel_level = 0.0
         self.fuel_capacity = 0.0
-        if json_data.get('FuelLevel') is not None:
-            self.fuel_level = json_data.get('FuelLevel').get('Value', 0.0)
-        if json_data.get('VirtualFuelLevel') is not None:
-            self.virtual_fuel_level = json_data.get('VirtualFuelLevel').get('Value', 0.0)
-        if json_data.get('FuelCapacity') is not None:
-            self.fuel_capacity = json_data.get('FuelCapacity').get('Value', 0.0)
+        fuel_level_dict = json_data.get('FuelLevel', None)
+        if fuel_level_dict is not None:
+            self.fuel_level = fuel_level_dict.get('Value', 0.0)
+        virtual_fuel_lvl_dict = json_data.get('VirtualFuelLevel', None)
+        if virtual_fuel_lvl_dict is not None:
+            self.virtual_fuel_level = virtual_fuel_lvl_dict.get('Value', 0.0)
+        fuel_cap_dict = json_data.get('FuelCapacity', None)
+        if fuel_cap_dict is not None:
+            self.fuel_capacity = fuel_cap_dict.get('Value', 0.0)
 
     def getattribute(self, name: str, default=''):
         return getattr(self, name, default)
