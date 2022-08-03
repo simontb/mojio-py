a3343cf2bd785f4c6b1d24f1b8822556d224128e
Karl Moos <karlmoos@gmail.com>
2022-08-03 06:58:03 -0500

    Minor refactors to API

--- mojio_sdk/api.py
+++ mojio_sdk/api.py
@@ -21,8 +21,6 @@ class API:
             return self._authentication.is_valid()
 
     def login(self) -> bool:
-        auth_url = "https://" + self._tenant + "-identity.moj.io/oauth2/token"
-
         payload = {
             'grant_type': 'password',
             'username': self._username,
@@ -32,30 +30,33 @@ class API:
             'scope': 'full offline_access'
         }
 
-        response = requests.post(auth_url, data=payload)
+        response = requests.post(self._get_auth_endpoint(), data=payload)
 
         self._authentication = Authentication()
         self._authentication.set_json(response.json())
         return self.logged_in()
 
     def get_vehicles(self) -> list[Vehicle]:
-        return self._parse_devices(self._get_response(self._get_endpoint("vehicles")))
+        return self._parse_devices(self._get_response("vehicles"))
 
     def get_trips(self) -> list[Trip]:
-        return self._parse_trips(self._get_response(self._get_endpoint("trips")))
+        return self._parse_trips(self._get_response("trips"))
 
     def get_trip(self, trip_id: str) -> Trip:
-        return Trip(self._get_response(self._get_endpoint("%s/%s" % ("trips", trip_id))))
+        return Trip(self._get_response("%s/%s" % ("trips", trip_id)))
 
-    def _get_endpoint(self, service: str) -> str:
+    def _get_api_endpoint(self, service: str) -> str:
         return "https://%s-api.moj.io/v2/%s" % (self._tenant, service)
 
-    def _get_response(self, data_url: str) -> dict:
+    def _get_auth_endpoint(self) -> str:
+        return "https://%s-identity.moj.io/oauth2/token" % (self._tenant)
+
+    def _get_response(self, service: str) -> dict:
         if self._authentication is None or not self._authentication.is_valid():
             self.login()
 
         auth_header = self._authentication.create_header()
-        response = requests.get(data_url, headers=auth_header)
+        response = requests.get(self._get_api_endpoint(service), headers=auth_header)
         return response.json()
 
     @staticmethod
